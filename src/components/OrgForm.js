f68601e8660d08fe1193488a640cbf90b1a69803
Lance B <bhmtnbiker@hotmail.com>
2019-01-24 11:33:06 -0600

    Updated to message board and form

--- src/components/OrgForm.js
+++ src/components/OrgForm.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Styles from '../styles/OrgFormStyles'
 import { Form, Field } from 'react-final-form'
 
+
 // eslint-disable-next-line
 //import Cards from 'Cards';
 
@@ -26,7 +27,7 @@ const OrgForm = () => (
           <div>
             <label>Organization</label>
             <Field
-              name="firstName"
+              name="organization"
               component="input"
               type="text"
               placeholder="Enter Organization Name"
@@ -35,7 +36,7 @@ const OrgForm = () => (
           <div>
             <label>Disaster</label>
             <Field
-              name="lastName"
+              name="disaster"
               component="input"
               type="text"
               placeholder="Enter Disaster Name"
@@ -43,121 +44,26 @@ const OrgForm = () => (
           </div>
           <div>
             <label>18+</label>
-            <Field name="employed" component="input" type="checkbox" />
+            <Field name="is18Up" component="input" type="checkbox" />
           </div>
-          {/* <div>
-            <label>Favorite Color</label>
-            <Field name="favoriteColor" component="select">
-              <option />
-              <option value="#ff0000">Red</option>
-              <option value="#00ff00">Green</option>
-              <option value="#0000ff">Blue</option>
-            </Field>
-          </div> */}
-          {/* <div>
-            <label>Toppings</label>
-            <Field name="toppings" component="select" multiple>
-              <option value="chicken"> Chicken</option>
-              <option value="ham"> Ham</option>
-              <option value="mushrooms"> Mushrooms</option>
-              <option value="cheese"> Cheese</option>
-              <option value="tuna"> Tuna</option>
-              <option value="pineapple">Pineapple</option>
-            </Field>
-          </div> */}
-          {/* <div>
-            <label>Sauces</label>
-            <div>
-              <label>
-                <Field
-                  name="sauces"
-                  component="input"
-                  type="checkbox"
-                  value="ketchup"
-                />{' '}
-                Ketchup
-              </label>
-              <label>
-                <Field
-                  name="sauces"
-                  component="input"
-                  type="checkbox"
-                  value="mustard"
-                />{' '}
-                Mustard
-              </label>
-              <label>
-                <Field
-                  name="sauces"
-                  component="input"
-                  type="checkbox"
-                  value="mayonnaise"
-                />{' '}
-                Mayonnaise
-              </label>
-              <label>
-                <Field
-                  name="sauces"
-                  component="input"
-                  type="checkbox"
-                  value="guacamole"
-                />{' '}
-                Guacamole 🥑
-              </label>
-            </div>
-          </div> */}
-          {/* <div>
-            <label>Best Stooge</label>
-            <div>
-              <label>
-                <Field
-                  name="stooge"
-                  component="input"
-                  type="radio"
-                  value="larry"
-                />{' '}
-                Larry
-              </label>
-              <label>
-                <Field
-                  name="stooge"
-                  component="input"
-                  type="radio"
-                  value="moe"
-                />{' '}
-                Moe
-              </label>
-              <label>
-                <Field
-                  name="stooge"
-                  component="input"
-                  type="radio"
-                  value="curly"
-                />{' '}
-                Curly
-              </label>
-            </div>
-          </div> */}
+          <div>
+            <label>Ongoing</label>
+            <Field name="isOngoing" component="input" type="checkbox" />
+          </div>
           <div>
             <label>Rally Point</label>
             <Field name="rallyPoint" component="input" text="text" placeholder="Rally Point Address" />
           </div>
           <div>
             <label>Details</label>
-            <Field name="details" component="input" type="text" placeholder="Enter Specific Details Here" />
+            <Field name="details" component="textarea" placeholder="Enter Specific Details Here" />
           </div>
           
           <div className="buttons">
             <button type="submit" disabled={submitting || pristine}>
               Submit
             </button>
-            {/* <button
-              type="button"
-              onClick={form.reset}
-              disabled={submitting || pristine}
-            >
-              Reset
-            </button> */}
+            
           </div>
           
         </form>
