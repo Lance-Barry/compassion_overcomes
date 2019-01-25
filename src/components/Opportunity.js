01b4f806992ec459a897d541686541f8730603a7
Lance B <bhmtnbiker@hotmail.com>
2019-01-25 11:03:24 -0600

    Bug fix with opportunities not showing up

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Opportunity = ({ opportunity }) => (
   <div style={{margin: "50px auto", padding: "50px", marginTop: "80px", textAlign: "center", border: "5px solid black"}}>
-    <h3>Organization: { opportunity.Organization }</h3>
-    <p>Disaster Name: { opportunity.DisasterName }</p>
+    <h3>Organization: { opportunity.organization }</h3>
+    <p>Disaster Name: { opportunity.disaster }</p>
     <p>Active: { opportunity.isActive ? "Yes" : "Ended"}</p>
-    <p>Rally Point: { opportunity.RallyPoint}</p>
-    <p>Details: { opportunity.Details}</p>
+    <p>Rally Point: { opportunity.rallyPoint}</p>
+    <p>Details: { opportunity.details}</p>
   </div>
 )
 
