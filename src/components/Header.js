5758a2b2a4fd459c95f47cb36c0fd0ff0b8d3b42
Lance B <bhmtnbiker@hotmail.com>
2019-02-02 21:35:55 -0600

    Opportunity Board Responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import HeaderStyles from "../styles/HeaderStyles";
 import '../styles/header.css';
 
 const Header = () => (
-    <div className="menuStyling">
-        <div style={HeaderStyles}>
+    <div id="myNav" className="menuStyling">
+        <div className="overlay-content" style={HeaderStyles}>
             <Link className="nav-button" to="/">Home |</Link>
             <Link className="nav-button" to="/about"> About |</Link>
             <Link className="nav-button" to="/organizations"> Organizations |</Link>
--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/opportunityBoard.css';
 
 const Opportunity = ({ opportunity }) => (
+  <div className='container'>
   <div className="oppJS">
     <h4>Organization: { opportunity.Organizations }</h4>
     <p>Disaster Name: { opportunity.Disaster }</p>
     <p>Active: { opportunity.isActive ? "Yes" : "Ended"}</p>
     <p>Rally Point: { opportunity.RallyPoint}</p>
     <p>Details: { opportunity.Details}</p>
+  </div>
   </div>
 )
 
